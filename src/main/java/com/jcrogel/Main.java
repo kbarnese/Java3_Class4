a208b34ab7c6a1cf29d84182e632dc5b20eb7789
Juan C Moreno <moreno@JCs-Personal-Macbook-Pro.local>
2019-01-25 23:13:29 -0800

    Initial commit

---
+++ src/main/java/com/jcrogel/Main.java
@@ -0,0 +1,30 @@
+package com.jcrogel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        String fxmlPath = "/fxml/sample.fxml";
+        FXMLLoader loader = new FXMLLoader( getClass().getResource( fxmlPath ) );
+        Parent root = loader.load();
+
+        primaryStage.setTitle("Tweet Stuff");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+
+        Controller c = loader.getController();
+        c.load();
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/test/java/com/jcrogel/FaceFinderTest.java
@@ -0,0 +1,14 @@
+package com.jcrogel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FaceFinderTest {
+    @Test
+    public void testStuff(){
+        System.out.println("Tested");
+
+    }
+
+}
